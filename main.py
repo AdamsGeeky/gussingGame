07d89d9464d8fe503a0483da1583a0ab2e1e589a
ADAMU MUHAMMAD MUHAMMAD <muhdadamu0008@gmail.com>
2023-09-27 16:18:47 +0100

    feat: winners Award

--- main.py
+++ main.py
@@ -3,9 +3,44 @@ import random
 import time
 # Import the 'pygame' library for game development and multimedia applications
 import pygame
+from pygame.locals import *  # Import QUIT and other constants
 
 # Initialize the pygame library
 pygame.init()
+
+def display_winners_award():
+    # Set up the window
+    screen_width = 800
+    screen_height = 600
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("Winner's Award")
+    
+    # Load the award image
+    award_image = pygame.image.load("award.png")  # Replace "winner_award.png" with the actual image file
+    
+    # Get the size of the image
+    image_width, image_height = award_image.get_size()
+    # Calculate the position to center the image
+    x = (screen_width - image_width) // 2
+    y = (screen_height - image_height) // 2
+
+    # Main loop for displaying the image
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+        
+     # Clear the screen
+        screen.fill((255, 255, 255))  # Fill with white background color
+        
+        # Display the award image centered on the screen
+        screen.blit(award_image, (x, y))
+        
+        pygame.display.update()
+    
+    pygame.quit()
+
 
 # Load sound files using pygame's mixer module
 # 'pygame.mixer.Sound' is used to load and manage sound effects and music in the game.
@@ -103,6 +138,7 @@ def guess_the_number(level):
     if level == 3:
         print(f"{GREEN}🏆 Congratulations! You have completed all levels!")
         play_correct_sound()
+        display_winners_award()
         print("📲 The next version of the game will be Android-based. Stay tuned!")
         time.sleep(5)
     else:
